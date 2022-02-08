965b9efdbcc053843edc2c79a7ea4c1a9341a63d
PavlovVasiliy <vasylyjpavlov@mail.ru>
2022-02-08 23:21:42 +0300

    Task_2_1_4 Done

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,9 @@
 package koschei.models;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "singleton")
 public class Deth8 {
 }
